636ebdc87ef4b948ae2847d60b572277579a9404
gabriellloc <gabrielocdev595@gmail.com>
2025-10-14 23:44:55 -0300

    projects

--- js/projects.js
+++ js/projects.js
@@ -1,5 +1,7 @@
 const allProjects = document.querySelectorAll(".project")
 
+const contentProjectsDiv = document.querySelectorAll(".contentProjects > div")
+
 allProjects.forEach(entries => {
 
   entries.addEventListener("mouseenter", () => {
@@ -16,34 +18,32 @@ allProjects.forEach(entries => {
     })
   })
 
-  const projectA = entries.querySelector("a")
-  projectA.addEventListener("click", (event) => {
-    event.preventDefault()
-  })
+  const projectA = entries.querySelector("button")
+  projectA.disabled = 'true'
   projectA.addEventListener("mouseenter", (event) => {
     const desc = entries.querySelector(".hiddenDesc")
-    console.log(desc)
     desc.style.height = desc.scrollHeight + "px"
     desc.style.opacity = "1"
 
     projectA.textContent = "Conhecer o Projeto"
     projectA.classList.add("meetProject")
 
-    projectA.addEventListener("click", () => {
-      window.location.href = projectA.href
-    })
+    
+    projectA.removeAttribute("disabled")
   })
 
   entries.addEventListener("mouseleave", () => {
     const desc = entries.querySelector(".hiddenDesc")
     // desc.classList.add("hiddenDesc")
-    desc.style.opacity = "0"
     desc.style.height = "0"
+    desc.style.opacity = "0"
 
     projectA.textContent = "Mais informações"
     projectA.classList.remove("meetProject")
-    projectA.addEventListener("click", (event) => {
-      event.preventDefault()
-    })
+    projectA.disabled = 'true'
   })
+})
+
+contentProjectsDiv.forEach(div => {
+  div.style.height = (29.6875) + "rem"
 })
